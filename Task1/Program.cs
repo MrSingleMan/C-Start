0611aa3a9c255b204f1bf20dc7b3293d2f8d069a
Alexander <n00mer00k@gmail.com>
2024-04-08 15:58:04 +0400

    add new some variables lesson1:task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello world"); 
+﻿// Console.WriteLine("Hello world"); 
+
+int value = 50;
+Console.WriteLine(value);
+
+double d = 6.5;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
